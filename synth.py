db05035560f44f5d0f050bc592335abeeb328ea1
Jonathan Lui <jonathanlui@google.com>
2018-06-26 15:32:17 -0700

    fix: check in synth tool; update gax dep

---
+++ synth.py
@@ -0,0 +1,32 @@
+import synthtool as s
+import synthtool.gcp as gcp
+import logging
+from pathlib import Path
+import subprocess
+
+logging.basicConfig(level=logging.DEBUG)
+
+gapic = gcp.GAPICGenerator()
+
+versions = ['v1']
+
+for version in versions:
+    library = gapic.node_library(
+        'container', version,
+        config_path="/google/container/"
+                    "artman_container.yaml")
+
+    # skip index, protos, package.json, and README.md
+    s.copy(
+        library,
+        excludes=['package.json', 'README.md', 'src/index.js'],
+    )
+
+#
+# Node.js specific cleanup
+#
+subprocess.run(['npm', 'install'])
+
+# # prettify and lint
+subprocess.run(['npm', 'run', 'prettier'])
+subprocess.run(['npm', 'run', 'lint'])
