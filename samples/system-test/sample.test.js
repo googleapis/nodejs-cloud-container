f015b0b6a213372c3de31431598ec9ad04a03c49
Jonathan Lui <jonathanlui@google.com>
2019-04-04 17:41:24 -0700

    fix

--- samples/system-test/sample.test.js
+++ samples/system-test/sample.test.js
@@ -16,17 +16,9 @@
 'use strict';
 
 const {assert} = require('chai');
-<<<<<<< HEAD
 const cp = require('child_process');
 
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
-=======
-const cp = require('child_process');
->>>>>>> master
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('container samples', () => {
   it('should run the quickstart', async () => {
